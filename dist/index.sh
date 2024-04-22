f1aeed064fca4eb950744094da3a6e53b2e9ceba
Hauke5 <haukeschmidt@gmail.com>
2024-04-22 11:35:49 -0700

    version patch

--- dist/index.sh
+++ dist/index.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo $dirname
 
-# npx @hauke5/nextjs-utils@latest
-# cp -R $(dirname $0)/../@hauke5/dialog/dist/app ./
+cp -R $(dirname $0)/../@hauke5/dialog/dist/example/* ./
 # cp -R $(dirname $0)/../@hauke5/dialog/dist/lib ./
 
 echo "type `npm run dev`, then go to htp://localhost:3000/examples/dialog"
