635131a70cea94185983a2cb25ccdb3a32ac93e9
Hauke5 <haukeschmidt@gmail.com>
2024-04-22 12:33:22 -0700

    version patch

--- dist/index.sh
+++ dist/index.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-echo ">>>"$dirname
-echo ">>>>"$(dirname $0)
+echo $(dirname $0) # should show ~/.npm/_npx/.../node_modules/.bin
 
-cp -R $(dirname $0)/../@hauke5/dialog/dist/example/* ./dialog/
-# cp -R $(dirname $0)/../@hauke5/dialog/dist/lib ./
+Bundle="dialog"
+mkdir -p ./$Bundle
+cd ./$Bundle
+cp -R $(dirname $0)/../@hauke5/$Bundle/dist/example/* ./
+cp -R $(dirname $0)/../@hauke5/$Bundle/dist/lib ./
 
-
-echo "type 'npm run dev', then go to htp://localhost:3000/examples/dialog"
+pwd
+npm i
+cp -R ./node_modules/@hauke5/utils/dist/lib ./
+cp -R ./node_modules/@hauke5/nextjs-utils/dist/lib ./
+npm run dev
+echo "type 'cd ./$Bundle', then 'npm run dev', then point a browser to htp://localhost:3000/examples/"$bundle
