4141246f062ec4ab905a578ec49851cfef093c40
Hauke5 <haukeschmidt@gmail.com>
2024-04-22 11:53:32 -0700

    version patch

--- dist/index.sh
+++ dist/index.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo $dirname
+echo ">>>"$dirname
+echo ">>>>"$(dirname $0)
 
-cp -R $(dirname $0)/../@hauke5/dialog/dist/example/* ./
+cp -R $(dirname $0)/../@hauke5/dialog/dist/example/* ./dialog/
 # cp -R $(dirname $0)/../@hauke5/dialog/dist/lib ./
 
-echo "type `npm run dev`, then go to htp://localhost:3000/examples/dialog"
+
+echo "type 'npm run dev', then go to htp://localhost:3000/examples/dialog"
