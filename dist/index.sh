387a4eec9f1b33354f58988d6572d9dfd6670a80
Hauke5 <haukeschmidt@gmail.com>
2024-04-24 10:48:02 -0700

    version patch

--- dist/index.sh
+++ dist/index.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # $(dirname $0) should point to ~/.npm/_npx/.../node_modules/.bin
 
-Bundle="dialog"
+Bundle="Dialog"
+bundle="dialog"
 mkdir -p ./$Bundle
 cd ./$Bundle
-cp -R $(dirname $0)/../@hauke5/$Bundle/dist/example/* ./
-cp -R $(dirname $0)/../@hauke5/$Bundle/dist/lib ./
+cp -R $(dirname $0)/../@hauke5/$bundle/dist/example/* ./
+cp -R $(dirname $0)/../@hauke5/$bundle/dist/lib ./
 
 npm i
 cp -R ./node_modules/@hauke5/utils/dist/lib ./
