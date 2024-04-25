b11313f6616bb912addc7709a3495f03ae511325
Hauke5 <haukeschmidt@gmail.com>
2024-04-24 18:02:12 -0700

    version patch

--- runExample.sh
+++ runExample.sh
@@ -10,5 +10,6 @@ cp -R $(dirname $0)/../@hauke5/$bundle/ ./
 npm i
 # cp -R ./node_modules/@hauke5/utils/lib ./
 # cp -R ./node_modules/@hauke5/nextjs-utils/lib ./
-echo "point a browser to http://localhost:3000/examples/"$Bundle
-npm run dev
+echo "point a browser to http://localhost:3000/example
+npm run build
+npm start
