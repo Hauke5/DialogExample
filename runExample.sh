4524465672da79ad16c1e191d0158720de7763cc
Hauke5 <haukeschmidt@gmail.com>
2024-10-20 11:28:43 -0700

    version "1.1.6"

---
+++ runExample.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+# call from the runtime ./Dialog folder as:
+# ./node_modules/@hauke5/dialog/runExample.sh
+
+# Bundle="Dialog"
+# bundle="dialog"
+# dirName=$(dirname $0)
+
+# echo "NODE_ENV=$NODE_ENV"
+
+# pwd=$(pwd)
+# npmV=$(npm pkg get version)
+
+# echo "npm=$npmV, cwd=$pwd"
+
+# appDir="./node_modules/@hauke5/$bundle/"
+# echo "copying Dialog from $appDir to ./"
+# cp -R $appDir ./
+
+# npm i
+# npm run build
+
+git clone https://github.com/Hauke5/Dialog.git 
+cd Dialog
+npm i
+# npm run dev
