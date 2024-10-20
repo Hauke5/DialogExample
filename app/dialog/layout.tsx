93fee31c51fb5c224e109dca13109dd5065373e1
Hauke5 <haukeschmidt@gmail.com>
2024-10-20 14:30:18 -0700

    version "1.2.0"

--- app/dialog/layout.tsx
+++ app/dialog/layout.tsx
@@ -1,6 +1,6 @@
-import { LayoutProps }  from "dialog/app/layout";
 import { AppTitleBar }  from "@hauke5/lib/apps/AppTitleBar";
 import styles           from './examples.module.scss'
+import { LayoutProps }  from '../../app/layout'
 
 export default function ExampleLayout({children}:LayoutProps) {
    return <div className={styles.exampleLayout}>
